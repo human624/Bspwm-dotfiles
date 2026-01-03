0aa46b1bf67a4b183837faa893c9d58d733e8547
human624 <htr654509@gmail.com>
2026-01-03 15:00:40 +0500

    Update install_tasks.py and Translate for English

--- Installer/core/install_tasks.py
+++ Installer/core/install_tasks.py
@@ -13,7 +13,7 @@ def install_pacman_packages():
 
     pkg = os.path.join(BASE_DIR, "packages.txt")
     if not os.path.isfile(pkg):
-        log_err("packages.txt не найден")
+        log_err("packages.txt not found")
         return
 
     with open(pkg) as f:
@@ -27,12 +27,16 @@ def install_ohmyzsh():
     clear()
     header("OH MY ZSH")
 
+    # Download and run Oh My Zsh installer
     run("wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh")
     run("sh install.sh")
 
     zsh_custom = os.environ.get("ZSH_CUSTOM", f"{HOME}/.oh-my-zsh/custom")
 
+    # Install Powerlevel10k theme
     run(f"git clone --depth=1 https://github.com/romkatv/powerlevel10k.git {zsh_custom}/themes/powerlevel10k")
+    
+    # Install Zsh plugins
     run(f"git clone https://github.com/zsh-users/zsh-autosuggestions {zsh_custom}/plugins/zsh-autosuggestions")
     run(f"git clone https://github.com/zsh-users/zsh-syntax-highlighting {zsh_custom}/plugins/zsh-syntax-highlighting")
 
@@ -53,7 +57,7 @@ def install_aur_packages():
 
     pkg = os.path.join(BASE_DIR, "aur_packages.txt")
     if not os.path.isfile(pkg):
-        log_err("aur_packages.txt не найден")
+        log_err("aur_packages.txt not found")
         return
 
     with open(pkg) as f:
