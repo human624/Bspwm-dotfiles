7b720290c173bcfd53d4cceb4610b2bda3ed9aa3
human624 <htr654509@gmail.com>
2026-03-24 14:55:17 +0500

    Update change_language.sh

--- .local/Bspwm/bin/change_language.sh
+++ .local/Bspwm/bin/change_language.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-CURRENT_LAYOUT=$(xset -q|grep LED| awk '{ print $10 }')
+CURRENT_LAYOUT=$(xset -q | grep LED | awk '{ print $10 }')
 
-setxkbmap -layout us,ru -option "grp:alt_shift_toggle"
-if [ "$CURRENT_LAYOUT" = "00000000" ]; then
-	notify-send "Lang: US" -t 700
-fi
+setxkbmap -layout us,ru -option "grp:alt_shift_toggle,ctrl:nocaps"
 
-if [ "$CURRENT_LAYOUT" = "00001000" ]; then
-    notify-send "Lang: RU" -t 700
-fi
+case "$CURRENT_LAYOUT" in
+    "00000000") notify-send "Lang: US" -t 700 ;;
+    "00001000") notify-send "Lang: RU" -t 700 ;;
+esac
